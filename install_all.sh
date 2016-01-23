a938de40377f978ce9640fdc82c37d5d3b2a8d00
zultuta <zulqordnaingambari@yahoo.co.uk>
2016-01-24 00:43:24 +0800

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -83,7 +83,7 @@ echo
 git clone -b v0.35.0  --depth 1 https://github.com/facebook/folly.git
 git clone -b v0.24.0  --depth 1 https://github.com/facebook/fbthrift.git
 git clone https://github.com/facebook/thpp
-git clone https://github.com/facebook/fblualib
+git clone https://github.com/zultuta/fblualib
 
 echo
 echo Building folly
